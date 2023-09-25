785b32f40c88aee35627464555616d1eb5176733
Zombie <73972020+Shiro4k@users.noreply.github.com>
2023-09-25 21:56:43 +0800

    Task 54

---
+++ Task 54/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+ */
+
+int SetNumber(string message)
+{
+    System.Console.Write($"Enter number {message}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int[,] GetMatrix(int rows, int columns, int minValue = 10, int maxValue = 99)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+            matrix[i, l] = rand.Next(minValue, maxValue + 1);
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+            Console.Write($"{matrix[i, l]} ");
+        Console.WriteLine();
+    }
+}
+void SortMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.Clear();
+int rows = SetNumber("m");
+int columns = SetNumber("n");
+int[,] array = GetMatrix(rows, columns, 0, 9);
+PrintMatrix(array);
+System.Console.WriteLine();
+SortMatrix(array);
+PrintMatrix(array);
